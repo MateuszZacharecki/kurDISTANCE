1714e9cb7ed0999e40952a351bd953413d2de44b
Mateussz Zacharecki <mateusz.zacharecki1@gmail.com>
2026-05-29 22:56:50 +0200

    Add lock-step measures & math utils

---
+++ kurDISTANCE/src/math_utils.h
@@ -0,0 +1,17 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+#include <stddef.h>
+
+#define ln_2 0.6931471805599453
+
+double _abs(double x);
+double _square(double x);
+double _sqrt(double x);
+double _pow(double x, size_t p);
+double _sqrt_p(double x, size_t p);
+double _log(double x);
+double _min(double x, double y);
+double _max(double x, double y);
+
+#endif
